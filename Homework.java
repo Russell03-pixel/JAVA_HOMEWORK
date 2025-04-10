be380258a8fa2d0b63be333b0bc83c9b6b0da6a1
Russell03-pixel <russelltatenda95@gmail.com>
2025-04-10 11:00:11 +0200

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,64 @@
+public class Homework {
+    public static void main(String[] args) {
+     String city = "Pretoria";
+     System.out.println(city.toUppercase());
+
+     String favfood = "pussy";
+      System.out.println(favfood.length());
+
+      String name = "Gringo";
+      System.out.println(name.charAt(0));
+     
+         //ARRAYS MODULE
+      Sring [] movies = {"Takers", "Deadpool", "Avengers", "Titanic"};
+      System.out.println("second movie:" + movies[1]);
+
+      int[] numbers = {9, 18, 27, 36, 45};
+      System.out.println("Last number:" + numbers[numbers.length -1]);
+        
+        numbers[0] = 99;
+        System.out.println("First number:" + numbers[0]);
+        for (int i =0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        } 
+
+        //LOOPS MODULE
+        System.out.println("Print name 3 times:")
+        for (int i = 0; i < 3; i++){
+            System.out.println("Gringo");
+        }
+        System.out.println("count 1 to 5 using while loop:");
+        int count = 1;
+        while (count <=5){
+            System.out.println(cout);
+            count++;
+        } 
+        System.out.println("coutn down from 5 to 1 using do while loop:");
+        int countdown = 5;
+        do {
+            System.out.println(countdown):
+            countdown--;
+        } while (countdown >=1);
+
+        //CHAR MODULE
+        char initial = 'R';
+        System.out.println("Initial:" + initial);
+        if (initial == 'R'){
+            System.out.println("Initial is R.");
+        } else {
+            System.out.println("initial is not R.");
+        } 
+        char a= 'G';
+        char b= 'H';
+        if (a > b){
+            System.out.println("a is greater than b.");
+        } esle if (a < b){
+            System.out.println("b is greater than a.");
+        } else {
+            System.out.println("a is equal to b.");
+        }
+        }
+        }
+        
+
+    }   
